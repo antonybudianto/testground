c9a2848acf7085fdbf467d29070e29a09634df43
Antony Budianto <antonybudianto@gmail.com>
2018-05-31 14:28:50 +0700

    show source style object

--- src/components/SourceView.js
+++ src/components/SourceView.js
@@ -4,16 +4,20 @@ class SourceView extends Component {
   render () {
     const { element } = this.props
     console.log(element.style)
-    const arr = Object.entries(element.style).filter(a => a[1] && isNaN(a[0]))
-    console.log(arr)
+    const style = Object.entries(element.style).filter(a => a[1] && isNaN(a[0]))
+      .reduce((prev, curr) => {
+        return {
+          ...prev,
+          [curr[0]]: curr[1]
+        }
+      }, {})
+    const display = JSON.stringify(style, null, 2)
     return (
       <div style={{
         padding: '5px'
       }}>
         <strong className="basic-panel__subtitle">SOURCE</strong>
-        <p>
-          
-        </p>
+        <pre>{display}</pre>
       </div>
     )
   }
