42618e0b8da185b3716b4887d3c4f07f07f175d2
Antony Budianto <antonybudianto@gmail.com>
2018-05-31 15:10:48 +0700

    add clipboard to copy source

--- src/components/SourceView.js
+++ src/components/SourceView.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
 
 import { string as styleToString } from 'to-style'
+import Clipboard from 'react-clipboard.js'
 
 class SourceView extends Component {
   render () {
     const { element } = this.props
-    console.log(element.style)
     const style = Object.entries(element.style).filter(a => a[1] && isNaN(a[0]))
-      .reduce((prev, curr) => {
-        return {
-          ...prev,
-          [curr[0]]: curr[1]
-        }
-      }, {})
+    .reduce((prev, curr) => {
+      return {
+        ...prev,
+        [curr[0]]: curr[1]
+      }
+    }, {})
     const display = JSON.stringify(style, null, 2)
     let displayCss = styleToString(style).replace(/; /g, '; \n')
     displayCss = displayCss ? displayCss + ';' : ''
@@ -20,10 +20,16 @@ class SourceView extends Component {
       <div style={{
         padding: '5px'
       }}>
-        <strong className="basic-panel__subtitle">OBJECT</strong>
+        <strong className="basic-panel__subtitle">OBJECT</strong> &nbsp;
+        <Clipboard data-clipboard-text={display}>
+          <i className="fas fa-clipboard"></i>
+        </Clipboard>
         <pre>{display}</pre>
 
-        <strong className="basic-panel__subtitle">CSS</strong>
+        <strong className="basic-panel__subtitle">CSS</strong> &nbsp;
+        <Clipboard data-clipboard-text={displayCss}>
+          <i className="fas fa-clipboard"></i>
+        </Clipboard>
         <pre>{displayCss}</pre>
       </div>
     )
