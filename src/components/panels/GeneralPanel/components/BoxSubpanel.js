7c4d5c353844e617a78863c50bb8e212c483d9c3
Antony Budianto <antonybudianto@gmail.com>
2018-06-02 20:46:21 +0700

    add margin padding

--- src/components/panels/GeneralPanel/components/BoxSubpanel.js
+++ src/components/panels/GeneralPanel/components/BoxSubpanel.js
@@ -9,6 +9,8 @@ class BoxSubpanel extends Component {
 
     this.state = {
       style: {
+        margin: props.element.style.margin,
+        padding: props.element.style.padding,
         width: props.element.style.width,
         height: props.element.style.height,
         backgroundColor: props.element.style.backgroundColor,
@@ -51,6 +53,34 @@ class BoxSubpanel extends Component {
                 this.setState(handleChangeStyle('backgroundColor', val, false));
               }}
               color={this.state.style.backgroundColor || ''}
+            />
+          </div>
+        </div>
+        <div className="flex items-center">
+          <div className="basic-box p1">
+            Margin: &nbsp;
+            <input
+              type="text"
+              placeholder="All"
+              value={this.state.style.margin}
+              onChange={e =>
+                this.setState(handleChangeStyle('margin', e.target.value))
+              }
+              style={{ width: 35 }}
+              className="general-panel__input"
+            />
+          </div>
+          <div className="basic-box p1">
+            Padding: &nbsp;
+            <input
+              type="text"
+              placeholder="All"
+              value={this.state.style.padding}
+              onChange={e =>
+                this.setState(handleChangeStyle('padding', e.target.value))
+              }
+              style={{ width: 35 }}
+              className="general-panel__input"
             />
           </div>
         </div>
