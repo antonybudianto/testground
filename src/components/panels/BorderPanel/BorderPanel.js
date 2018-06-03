4453a15afc2193c7086d2bb6683518192287a66a
Antony Budianto <antonybudianto@gmail.com>
2018-06-03 16:29:19 +0700

    init state

--- src/components/panels/BorderPanel/BorderPanel.js
+++ src/components/panels/BorderPanel/BorderPanel.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import BasicPanel from '../BasicPanel';
 
 class BorderPanel extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      style: {},
+    };
+  }
+
   render() {
     return (
       <BasicPanel title="BORDER">
