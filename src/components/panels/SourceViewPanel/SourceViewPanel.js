d899c4663f2d41318bee22b9d3f4f73006f0f8bb
Antony Budianto <antonybudianto@gmail.com>
2018-06-05 09:07:37 +0700

    properly generate css source

--- src/components/panels/SourceViewPanel/SourceViewPanel.js
+++ src/components/panels/SourceViewPanel/SourceViewPanel.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { string as styleToString } from 'to-style';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import Button from '../../../components/controls/Button';
@@ -8,32 +7,28 @@ import './SourceViewPanel.css';
 class SourceViewPanel extends Component {
   render() {
     const { element } = this.props;
-    const style = Object.entries(element.style)
-      .filter(a => a[1] && isNaN(a[0]) && a[1] !== 'initial')
-      .reduce((prev, curr) => {
-        return {
-          ...prev,
-          [curr[0]]: curr[1],
-        };
-      }, {});
-    const display = JSON.stringify(style, null, 2);
-    let displayCss = styleToString(style).replace(/; /g, '; \n');
-    displayCss = displayCss ? displayCss + ';' : '';
+    // const style = Object.entries(element.style)
+    // .filter(a => a[1] && isNaN(a[0]) && a[1] !== 'initial')
+    // .reduce((prev, curr) => {
+    //   return {
+    //     ...prev,
+    //     [curr[0]]: curr[1],
+    //   };
+    // }, {});
+    // const display = JSON.stringify(style, null, 2);
+    const styleStr = element.getAttribute('style')
+    let displayCss = styleStr.replace(/; /g, '; \n');
     return (
-      <div
-        style={{
-          padding: '5px',
-        }}
-      >
-        <div className="flex justify-between items-center">
+      <div className="p1">
+        {/* <div className="flex justify-between items-center">
           <strong className="basic-panel__subtitle">OBJECT</strong> &nbsp;
           <CopyToClipboard text={display}>
             <Button>
               <i className="fas fa-clipboard" />
             </Button>
           </CopyToClipboard>
-        </div>
-        <pre className="source-view__content">{display}</pre>
+        </div> */}
+        {/* <pre className="source-view__content">{display}</pre> */}
         <div className="flex justify-between items-center">
           <strong className="basic-panel__subtitle">CSS</strong> &nbsp;
           <CopyToClipboard text={displayCss}>
