8de31e65e36b763954e75219726f20d0032f62e5
Antony Budianto <antonybudianto@gmail.com>
2018-06-07 22:35:59 +0700

    refactor button sharedprops

--- src/components/controls/Button.js
+++ src/components/controls/Button.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const Button = props => (
-  <button
-    title={props.title}
-    onClick={props.onClick}
-    style={props.style}
-    disabled={props.disabled}
-    className={classNames('main-box__btn', props.className, {
-      'main-box__btn--icon': props.icon,
-      'main-box__btn--active': props.active,
-      'main-box__btn--sm': props.sm,
-    })}
-  >
-    {props.children}
-  </button>
-);
+const Button = props => {
+  const { title, onClick, style, disabled } = props;
+  const sharedProps = { title, onClick, style, disabled };
+  return (
+    <button
+      {...sharedProps}
+      className={classNames('main-box__btn', props.className, {
+        'main-box__btn--icon': props.icon,
+        'main-box__btn--active': props.active,
+        'main-box__btn--sm': props.sm,
+      })}
+    >
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
