2f8d6ef0b8bf2242fe805bd5b012f59336304fe8
Antony Budianto <antonybudianto@gmail.com>
2018-06-08 10:57:07 +0700

    remove commented code

--- src/components/panels/SourceViewPanel/SourceViewPanel.js
+++ src/components/panels/SourceViewPanel/SourceViewPanel.js
@@ -7,28 +7,10 @@ import './SourceViewPanel.css';
 class SourceViewPanel extends Component {
   render() {
     const { element } = this.props;
-    // const style = Object.entries(element.style)
-    // .filter(a => a[1] && isNaN(a[0]) && a[1] !== 'initial')
-    // .reduce((prev, curr) => {
-    //   return {
-    //     ...prev,
-    //     [curr[0]]: curr[1],
-    //   };
-    // }, {});
-    // const display = JSON.stringify(style, null, 2);
     const styleStr = element.getAttribute('style') || '';
     let displayCss = styleStr.replace(/;\s*/g, '; \n');
     return (
       <div className="p1">
-        {/* <div className="flex justify-between items-center">
-          <strong className="basic-panel__subtitle">OBJECT</strong> &nbsp;
-          <CopyToClipboard text={display}>
-            <Button>
-              <i className="fas fa-clipboard" />
-            </Button>
-          </CopyToClipboard>
-        </div> */}
-        {/* <pre className="source-view__content">{display}</pre> */}
         <div className="flex justify-between items-center">
           <strong className="basic-panel__subtitle">CSS</strong> &nbsp;
           <CopyToClipboard text={displayCss}>
