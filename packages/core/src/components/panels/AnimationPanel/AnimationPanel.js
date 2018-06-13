c84a9d57b822e62647036f243d789fbdc737a020
Antony Budianto <antonybudianto@gmail.com>
2018-06-13 15:11:10 +0700

    prepare basic anim panel

--- packages/core/src/components/panels/AnimationPanel/AnimationPanel.js
+++ packages/core/src/components/panels/AnimationPanel/AnimationPanel.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 
 import BasicPanel from '../BasicPanel';
+import Button from '../../controls/Button';
 
 export class AnimationPanel extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.element.setAttribute('style', {});
+  }
+
   render() {
     return (
       <BasicPanel title="ANIMATION">
-        <div>animation</div>
+        <div className="flex justify-center">
+          <div className="flex flex-column">
+            <Button icon>0%</Button>
+            <Button icon>100%</Button>
+          </div>
+          <div className="ml2">
+            <strong>CODE</strong>
+            <div className="mt1">sakit</div>
+          </div>
+        </div>
       </BasicPanel>
     );
   }
