4e7a9344389101a5794fcdc90751257daa3864a3
Antony Budianto <antonybudianto@gmail.com>
2018-06-15 19:08:39 +0700

    fix bug color update

--- packages/core/src/util/init-style.js
+++ packages/core/src/util/init-style.js
@@ -2,7 +2,7 @@ export function initStyle() {
   return (state, props) => {
     const style = window.getComputedStyle(props.element);
     const computedStyle = Object.keys(state.style).reduce((prev, key) => {
-      prev[key] = style[key];
+      prev[key] = style[key].replace('rgba(0, 0, 0, 0)', '');
       return prev;
     }, {});
     return {
